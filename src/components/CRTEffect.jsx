250b58b3306a46c5c974e4b8f5b7859343cca801
Titan648 <142923960+Titan648@users.noreply.github.com>
2025-11-08 19:20:32 +0300

    Create CRTEffect.jsx

---
+++ src/components/CRTEffect.jsx
@@ -0,0 +1,13 @@
+import './CRTEffect.css'
+
+function CRTEffect() {
+  return (
+    <>
+      <div className="crt-overlay"></div>
+      <div className="scanlines"></div>
+      <div className="screen-curve"></div>
+    </>
+  )
+}
+
+export default CRTEffect
